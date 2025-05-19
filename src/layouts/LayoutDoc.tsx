268066823d0409623fe41c80c9b0454a8ce9565e
CMartinez <devero@github.com>
2025-05-18 18:47:04 -0600

    fixed

--- src/layouts/LayoutDoc.tsx
+++ src/layouts/LayoutDoc.tsx
@@ -1,14 +1,14 @@
 import Header from '../Components/Header'
-import Aside from '../Components/Aside'
+import Aside from '../Components/Menu/Aside'
 import { Outlet } from 'react-router-dom'
 
 export default function LayoutDoc() {
     return (
-        <>  
-            <Header/>
+        <>
+            <Header />
             <div className='max-w-7xl py-3 px-12 mx-auto flex gap-10 mt-12 mb-4'>
-                <Aside/>
-                <Outlet/>
+                <Aside />
+                <Outlet />
             </div>
         </>
     )
