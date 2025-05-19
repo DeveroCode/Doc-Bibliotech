fce1dc2a6662350b0211658513d8b80c9e73b296
CMartinez <devero@github.com>
2025-05-18 20:19:52 -0600

    corrected  design

--- src/Components/NextPage.tsx
+++ src/Components/NextPage.tsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom"
 
 type NextPageProps = {
-    url?: string,
-    title?: string
+  url?: string,
+  title?: string
 }
-export default function NextPage({url, title} : NextPageProps) {
+export default function NextPage({ url, title }: NextPageProps) {
   return (
-    <div className="mt-3 pr-7">
-      <span className="text-gray-500 font-bold text-sm flex">Next</span>
-      <Link to={`/${url}`} className="text-xl font-bold">{title}</Link>
+    <div className="flex justify-end">
+      <div className="mt-3 pr-7">
+        <span className="text-gray-500 font-bold text-sm flex">Next</span>
+        <Link to={`/${url}`} className="text-xl font-bold">{title}</Link>
+      </div>
     </div>
   )
 }
